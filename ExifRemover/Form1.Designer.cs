4e5f16494ad74a81a64ed3acfae6d7c8a5c242e0
dido1092 <deyangospodinov1092@abv.bg>
2023-11-30 12:23:16 +0200

    New Vision

--- ExifRemover/Form1.Designer.cs
+++ ExifRemover/Form1.Designer.cs
@@ -31,7 +31,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             buttonAdd = new Button();
             buttonRemove = new Button();
-            buttonClear = new Button();
             buttonRemoveExif = new Button();
             richTextBoxImages = new RichTextBox();
             labelItems = new Label();
@@ -43,56 +42,49 @@
             // 
             // buttonAdd
             // 
+            buttonAdd.BackColor = SystemColors.GradientActiveCaption;
             buttonAdd.Location = new Point(38, 21);
             buttonAdd.Name = "buttonAdd";
-            buttonAdd.Size = new Size(66, 37);
+            buttonAdd.Size = new Size(421, 37);
             buttonAdd.TabIndex = 0;
-            buttonAdd.Text = "Add";
-            buttonAdd.UseVisualStyleBackColor = true;
+            buttonAdd.Text = "Select Images";
+            buttonAdd.UseVisualStyleBackColor = false;
             buttonAdd.Click += buttonAdd_Click;
             // 
             // buttonRemove
             // 
-            buttonRemove.Location = new Point(128, 21);
+            buttonRemove.BackColor = SystemColors.GradientActiveCaption;
+            buttonRemove.Location = new Point(465, 21);
             buttonRemove.Name = "buttonRemove";
-            buttonRemove.Size = new Size(66, 37);
+            buttonRemove.Size = new Size(78, 37);
             buttonRemove.TabIndex = 1;
             buttonRemove.Text = "Remove";
-            buttonRemove.UseVisualStyleBackColor = true;
+            buttonRemove.UseVisualStyleBackColor = false;
             buttonRemove.Click += buttonRemove_Click;
-            // 
-            // buttonClear
-            // 
-            buttonClear.Location = new Point(215, 21);
-            buttonClear.Name = "buttonClear";
-            buttonClear.Size = new Size(66, 37);
-            buttonClear.TabIndex = 2;
-            buttonClear.Text = "Clear";
-            buttonClear.UseVisualStyleBackColor = true;
-            buttonClear.Click += buttonClear_Click;
             // 
             // buttonRemoveExif
             // 
-            buttonRemoveExif.Location = new Point(793, 79);
+            buttonRemoveExif.BackColor = SystemColors.GradientActiveCaption;
+            buttonRemoveExif.Location = new Point(38, 582);
             buttonRemoveExif.Name = "buttonRemoveExif";
-            buttonRemoveExif.Size = new Size(108, 45);
+            buttonRemoveExif.Size = new Size(505, 45);
             buttonRemoveExif.TabIndex = 3;
-            buttonRemoveExif.Text = "Remove Exif";
-            buttonRemoveExif.UseVisualStyleBackColor = true;
+            buttonRemoveExif.Text = "Remove Exif Info";
+            buttonRemoveExif.UseVisualStyleBackColor = false;
             buttonRemoveExif.Click += buttonRemoveExif_Click;
             // 
             // richTextBoxImages
             // 
             richTextBoxImages.Location = new Point(38, 79);
             richTextBoxImages.Name = "richTextBoxImages";
-            richTextBoxImages.Size = new Size(738, 371);
+            richTextBoxImages.Size = new Size(505, 371);
             richTextBoxImages.TabIndex = 4;
             richTextBoxImages.Text = "";
             // 
             // labelItems
             // 
             labelItems.AutoSize = true;
-            labelItems.Location = new Point(709, 453);
+            labelItems.Location = new Point(477, 451);
             labelItems.Name = "labelItems";
             labelItems.Size = new Size(42, 15);
             labelItems.TabIndex = 5;
@@ -102,7 +94,7 @@
             // 
             textBoxDestination.Location = new Point(114, 497);
             textBoxDestination.Name = "textBoxDestination";
-            textBoxDestination.Size = new Size(509, 23);
+            textBoxDestination.Size = new Size(345, 23);
             textBoxDestination.TabIndex = 6;
             // 
             // label2
@@ -116,26 +108,28 @@
             // 
             // buttonBrowse
             // 
-            buttonBrowse.Location = new Point(629, 497);
+            buttonBrowse.BackColor = SystemColors.GradientActiveCaption;
+            buttonBrowse.Location = new Point(465, 497);
             buttonBrowse.Name = "buttonBrowse";
-            buttonBrowse.Size = new Size(75, 23);
+            buttonBrowse.Size = new Size(78, 23);
             buttonBrowse.TabIndex = 8;
             buttonBrowse.Text = "Browse...";
-            buttonBrowse.UseVisualStyleBackColor = true;
+            buttonBrowse.UseVisualStyleBackColor = false;
             buttonBrowse.Click += buttonBrowse_Click;
             // 
             // progressBarComplete
             // 
             progressBarComplete.Location = new Point(38, 456);
             progressBarComplete.Name = "progressBarComplete";
-            progressBarComplete.Size = new Size(585, 10);
+            progressBarComplete.Size = new Size(421, 10);
             progressBarComplete.TabIndex = 9;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(967, 566);
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(585, 663);
             Controls.Add(progressBarComplete);
             Controls.Add(buttonBrowse);
             Controls.Add(label2);
@@ -143,7 +137,6 @@
             Controls.Add(labelItems);
             Controls.Add(richTextBoxImages);
             Controls.Add(buttonRemoveExif);
-            Controls.Add(buttonClear);
             Controls.Add(buttonRemove);
             Controls.Add(buttonAdd);
             FormBorderStyle = FormBorderStyle.FixedSingle;
@@ -159,7 +152,6 @@
 
         private Button buttonAdd;
         private Button buttonRemove;
-        private Button buttonClear;
         private Button buttonRemoveExif;
         private RichTextBox richTextBoxImages;
         private Label labelItems;
